62527b9840b686d4c7c48158e63d324db5dbe564
kadeem254 <kadeemalexander254@gmail.com>
2023-03-05 17:11:38 +0300

    RESOLUTION
    Major change in used library, gsap/scrollTrigger used in place of scrollMagic. Initial interaction  makes me believe it might
    be a better library to us given my choice to use gsap animation library to which it is connected and built to intergrate with.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,77 @@
 import "./scss/pages/index.scss";
+
+import gsap from "gsap";
+import CSSPlugin from "gsap/CSSPlugin";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+import ScrollMagic from "scrollmagic";
+
+gsap.registerPlugin( CSSPlugin, ScrollTrigger );
+
+// PAGE OBJECTIVES
+// create controller
+// create scenes 
+// set value of scene based on viewport size
+
+// VARIABLES
+const MEDIA_QUERIES = {
+  "678": window.matchMedia( "(min-width: 678px)" ),
+}
+// const CONTROLLER = new ScrollMagic.Controller();
+
+const G_MM = gsap.matchMedia();
+
+G_MM.add(
+  "(min-width: 678px)",
+  ( ctx ) => {
+    let image_parallax = gsap.fromTo(
+      ".test-image",
+      {
+        yPercent: -75,
+      },
+      {
+        yPercent: 75,
+        ease: "none",
+        scrollTrigger: {
+          trigger: ".section-2",
+          start: "top bottom", // the default values
+          end: "bottom top",
+          scrub: true
+        }, 
+      }, 
+    )
+    return;
+  }
+)
+// gsap.fromTo(
+//   ".test-image",
+//   {
+//     yPercent: 75,
+//   },
+//   {
+//     yPercent: -75,
+//     ease: "none",
+//     scrollTrigger: {
+//       trigger: ".section-2",
+//       // start: "top bottom", // the default values
+//       // end: "bottom top",
+//       scrub: true
+//     }, 
+//   }
+// )
+
+
+
+window.addEventListener( "load", init );
+
+/**Runs once, when the page completes loading */
+function init(){
+  MEDIA_QUERIES[678].addEventListener( "change", pageResizeHandler );
+  return;
+}
+
+function pageResizeHandler( e ){
+  console.log( e.matches );
+  return;
+}
+
